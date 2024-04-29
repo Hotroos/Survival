ffa795ecd34a3ae66d695f213a0c629055381c24
Hotroos <156539417+Hotroos@users.noreply.github.com>
2024-04-29 08:01:29 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-                console.log('DOM fully loaded and parsed');
-            });
-function changeColor() {
-    var header = document.getElementById("header");
-    header.style.color = header.style.color == "blue" ? "black" : "blue";
-}
-
